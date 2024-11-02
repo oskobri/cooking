b7fefc3cec6f0004f0d8b620190adae3ff27d9c2
Boris Obrembski <oskobri@gmail.com>
2024-11-02 11:58:34 +0100

    fix: filament admin access prod

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,6 @@ use App\Enums\IngredientUnit;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('dev', function () {
+    $user = App\Models\User::first();
+    dd($user->email === config('filament.admin') && $this->hasVerifiedEmail());
 });
