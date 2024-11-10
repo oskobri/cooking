3b65a3fb11a9ffaa519547ad86ab628e80237286
Boris Obrembski <oskobri@gmail.com>
2024-11-10 18:16:29 +0100

    fix: recipes table

--- database/migrations/2024_09_04_173758_create_recipes_table.php
+++ database/migrations/2024_09_04_173758_create_recipes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
             $table->string('picture')->nullable();
             $table->string('url')->nullable();
             $table->string('source')->nullable();
-            $table->json('instructions')->nullable();
+            $table->text('instructions')->nullable();
             $table->unsignedSmallInteger('preparation_time')->nullable();
             $table->unsignedSmallInteger('total_time')->nullable();
             $table->unsignedSmallInteger('kcal')->nullable();
